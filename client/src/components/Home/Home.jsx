041b429610183cb6c74f3dc0c89008b94d756106
recover1988 <ericdenislaura@gmail.com>
2022-11-09 08:46:31 -0300

    subiendo

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import Pagination from "../Pagination/Pagination";
 import DogCard from "../DogCard/DogCard";
