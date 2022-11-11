17da05216680ad53fbc512a1de16160ca52c4957
recover1988 <ericdenislaura@gmail.com>
2022-11-11 17:55:45 -0300

    subiendo 17:55 11/11/2022

--- api/index.js
+++ api/index.js
@@ -22,5 +22,5 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT || 5000);
+  server.listen(process.env.PORT || 3001);
 });
